8112e05ee1111b75c9ed1c1d8ec790919292df6e
nbrabant <brabantnicolas59@gmail.com>
2019-06-04 22:39:09 +0200

    Move and reformat

--- GameRunner.php
+++ GameRunner.php
@@ -1,27 +1,22 @@
 <?php
 
-include __DIR__.'/Game.php';
+include __DIR__ . '/bootstrap/autoload.php';
 
-$notAWinner;
+$notAWinner = true;
 
-  $aGame = new Game();
-  
-  $aGame->add("Chet");
-  $aGame->add("Pat");
-  $aGame->add("Sue");
-  
-  
-  do {
-    
-    $aGame->roll(rand(0,5) + 1);
-    
-    if (rand(0,9) == 7) {
-      $notAWinner = $aGame->wrongAnswer();
+$aGame = new Game();
+
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
+
+do {
+    $aGame->roll(rand(0, 5) + 1);
+
+    if (rand(0, 9) == 7) {
+        $notAWinner = $aGame->wrongAnswer();
     } else {
-      $notAWinner = $aGame->wasCorrectlyAnswered();
+        $notAWinner = $aGame->wasCorrectlyAnswered();
     }
-    
-    
-    
-  } while ($notAWinner);
+} while ($notAWinner);
   
